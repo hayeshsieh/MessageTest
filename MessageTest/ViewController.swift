cb2618f6516b1b2cb146ad2a93d245f4672172e2
hayeshsieh <203418040@gms.tku.edu.tw>
2017-05-03 21:02:44 +0800

    Camera update

--- MessageTest/ViewController.swift
+++ MessageTest/ViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+
+
+
+class ViewController:UIViewController,
+        UIImagePickerControllerDelegate,
+        UINavigationControllerDelegate
+    {
+    
+    @IBOutlet weak var imagePicker: UIImageView!
+    
+    
+    @IBAction func openCameraButton(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else{
+            print("can't find camera");
+        }
+        func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+            print("didFinishPickingImage")
+            self.imagePicker.image = image // 到imageview顯示
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil) // 儲存圖片
+            picker.dismiss(animated: true, completion: nil) // 退出相機
+        }
+        
+        func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+            print("imagePickerControllerDidCancel")
+            picker.dismiss(animated: true, completion: nil) // 退出相機
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
