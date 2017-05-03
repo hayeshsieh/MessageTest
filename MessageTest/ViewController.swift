2e9b367fa65498c45b0253e851f8de54ba9123fa
hayeshsieh <203418040@gms.tku.edu.tw>
2017-05-03 15:55:23 +0800

    Initial Commit

---
+++ MessageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MessageTest
+//
+//  Created by 謝旭棠 on 2017/5/3.
+//  Copyright © 2017年 謝旭棠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
